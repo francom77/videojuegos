f0627f46816ac522ea783c9135b9b4c7408f9f55
francom77 <franco_m77@hotmail.com>
2014-12-19 17:53:55 -0300

    la puta que te pario franco

--- VirtualStreet/src/com/example/virtualstreet/MapActivity.java
+++ VirtualStreet/src/com/example/virtualstreet/MapActivity.java
@@ -27,7 +27,6 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.view.Window;
-import android.widget.ImageView;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesClient;
--- VirtualStreet/src/com/example/virtualstreet/ResultActivity.java
+++ VirtualStreet/src/com/example/virtualstreet/ResultActivity.java
@@ -7,30 +7,81 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.Window;
+import android.widget.Button;
 import android.widget.TextView;
 
+import com.facebook.UiLifecycleHelper;
+import com.facebook.widget.FacebookDialog;
 import com.loopj.android.http.JsonHttpResponseHandler;
 import com.loopj.android.http.RequestParams;
 
 public class ResultActivity extends Activity {
-	
+
 	private int zonaOrigen;
+	private UiLifecycleHelper uiHelper;
+	private String puntos;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		uiHelper = new UiLifecycleHelper(this, null);
+		uiHelper.onCreate(savedInstanceState);
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.activity_result);
 
 		Bundle bundle = getIntent().getExtras();
-		String puntos = bundle.getString("puntos");
+		puntos = bundle.getString("puntos");
 
 		TextView puntostxtvw = (TextView) findViewById(R.id.puntos_title);
 		puntostxtvw.setText("Tu puntaje es: " + puntos);
-		zonaOrigen =  getIntent().getExtras().getInt("zonaOrigen");
+		zonaOrigen = getIntent().getExtras().getInt("zonaOrigen");
 		registrarPuntaje(Integer.parseInt(puntos));
+		Button publishButton = (Button) findViewById(R.id.publishButton);
+		publishButton.setOnClickListener(new OnClickListener() {
 
+			@Override
+			public void onClick(View v) {
+				publishStory();
+
+			}
+
+		});
+
+	}
+
+	public void publishStory() {
+		FacebookDialog shareDialog = new FacebookDialog.ShareDialogBuilder(this)
+				.setName("Virtual Street")
+				.setDescription("He hecho "+puntos+" puntos. ¿Podras superarme?")
+				.setLink("http://virtualsback-uniquegames.rhcloud.com/explorer/").build();
+		uiHelper.trackPendingDialogCall(shareDialog.present());
+
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		uiHelper.onResume();
+	}
+
+	@Override
+	protected void onSaveInstanceState(Bundle outState) {
+		super.onSaveInstanceState(outState);
+		uiHelper.onSaveInstanceState(outState);
+	}
+
+	@Override
+	public void onPause() {
+		super.onPause();
+		uiHelper.onPause();
+	}
+
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+		uiHelper.onDestroy();
 	}
 
 	@Override
@@ -56,7 +107,7 @@ public class ResultActivity extends Activity {
 		finish();
 	}
 
-	public void reintentar(View view){
+	public void reintentar(View view) {
 		Intent i = new Intent(ResultActivity.this, LimpiacityActivity.class);
 		i.putExtra("zonaOrigen", zonaOrigen);
 
@@ -64,19 +115,16 @@ public class ResultActivity extends Activity {
 		finish();
 	}
 
-	public void registrarPuntaje(int puntaje){
+	public void registrarPuntaje(int puntaje) {
 		SharedPreferences sp = Prefs.getSharedPreferences(getApplication());
 		RequestParams params = new RequestParams();
 		params.put("usuarioIdusuario", sp.getInt("id", 100));
 		params.put("zonaIdzona", zonaOrigen);
 		params.put("puntaje", puntaje);
-		RestClient.put("UsuarioHasZonas", params, new JsonHttpResponseHandler(){
+		RestClient.put("UsuarioHasZonas", params,
+				new JsonHttpResponseHandler() {
 
-		});
+				});
 	}
 
-		
 }
-
-
-
